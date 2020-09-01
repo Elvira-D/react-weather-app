dcdc3ff8eb9663ec3a26789e9bd0750def95df46
Elvira-D <66033824+Elvira-D@users.noreply.github.com>
2020-09-01 17:01:42 +0100

    Updated links to GitHub & Linkedin

--- src/App.js
+++ src/App.js
@@ -6,10 +6,16 @@ export default function App() {
     return <div className = "App" >
         Weather App 
         <footer>
-            Coded by 
+            Coded by Elvira Di Marco
+            <br />
+            <a href="https://linkedin.com/in/elvira-di-marco" target="blank" rel="noopener noreferrer">
+                Linkedin
+            </a>
+            <br />
             <a href="https://github.com/Elvira-D/react-weather-app" target="blank" rel="noopener noreferrer">
-                Elvira Di Marco
+                GitHub
             </a>
         </footer>
         </div>;
 }
+
