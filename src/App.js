82ee621983c5be866734efec66c441f4b1ccfe8e
Elvira-D <66033824+Elvira-D@users.noreply.github.com>
2020-09-03 18:03:22 +0100

    Created Weather.js & Weather.css

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
-import React from 'react';
+import React from "react";
 
-import './App.css';
+import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
-    return <div className="App">
-        <div className="container"> </div>
-        <h1> Weather App </h1>
-        <footer>
-            Coded by Elvira Di Marco
-            <br />
-            <a href="https://linkedin.com/in/elvira-di-marco" target="blank" rel="noopener noreferrer">
-                Linkedin
-            </a>
-            <br />
-            <a href="https://github.com/Elvira-D/react-weather-app" target="blank" rel="noopener noreferrer">
-                GitHub
-            </a>
-        </footer>
-        </div>;
-}
+  return (
+    <div className="App">
+      <div className="container"> </div>
+      <h1> Weather App </h1>
+      <Weather />
 
+      <footer>
+        Coded by Elvira Di Marco
+        <br />
+        <a
+          href="https://linkedin.com/in/elvira-di-marco"
+          target="blank"
+          rel="noopener noreferrer"
+        >
+          Linkedin
+        </a>
+        <br />
+        <a
+          href="https://github.com/Elvira-D/react-weather-app"
+          target="blank"
+          rel="noopener noreferrer"
+        >
+          GitHub
+        </a>
+      </footer>
+    </div>
+  );
+}
---
+++ src/Weather.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  return <div className="Weather">
+      <h2> New York </h2>
+      <ul>
+          <li>Wednesday 7:30</li>
+          <li>
+              Mostly Sunny
+          </li>
+      </ul>
+          </div>;
+}
